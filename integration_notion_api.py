fadd91c026fad71cc6aa1eb9829fb6e333341223
Seyyid Yiğit <seyyid364@gmail.com>
2023-03-20 22:10:39 +0300

    Added class and function for create_database and create_page use NotionAPI.

---
+++ integration_notion_api.py
@@ -0,0 +1,28 @@
+import requests
+from datetime import datetime
+
+class NotionAPI:
+    def __init__(self, token, version="2022-02-22"):
+        self.token = token
+        self.version = version
+        self.headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+            "Notion-Version": self.version
+        }
+    
+    def create_database(self, data):
+        response = requests.post("https://api.notion.com/v1/databases", headers=self.headers, json=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Request failed with status {response.status_code}: {response.text}")
+            return None
+    
+    def create_page(self,data):
+        response = requests.post("https://api.notion.com/v1/pages", headers=self.headers, json=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Request failed with status {response.status_code}: {response.text}") 
+            return None
