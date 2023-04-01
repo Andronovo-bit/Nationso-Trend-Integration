59ed6591708a80d37f968cf216cc99acba09177b
Seyyid Yiğit <seyyid364@gmail.com>
2023-04-01 12:42:48 +0300

    Nation api url has maded global variable. So, you can change it easily.

--- integration_notion_api.py
+++ integration_notion_api.py
@@ -1,6 +1,4 @@
 import requests
-from datetime import datetime
-
 class NotionAPI:
     def __init__(self, token, version="2022-02-22"):
         self.token = token
@@ -10,9 +8,11 @@ class NotionAPI:
             "Content-Type": "application/json",
             "Notion-Version": self.version
         }
+        self.notion_api_url = "https://api.notion.com/v1/"
+
     
     def create_database(self, data):
-        response = requests.post("https://api.notion.com/v1/databases", headers=self.headers, json=data)
+        response = requests.post(self.notion_api_url + "databases", headers=self.headers, json=data)
         if response.status_code == 200:
             return response.json()
         else:
@@ -20,7 +20,7 @@ class NotionAPI:
             return None
     
     def create_page(self,data):
-        response = requests.post("https://api.notion.com/v1/pages", headers=self.headers, json=data)
+        response = requests.post(self.notion_api_url + "pages", headers=self.headers, json=data)
         if response.status_code == 200:
             return response.json()
         else:
