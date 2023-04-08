dd23910aa463f14cb82093bf042676e8c2eed296
Seyyid Yiğit <seyyid364@gmail.com>
2023-04-08 12:42:44 +0300

    Added medium scraper function. Additionally, Writed is a common get database id function, and edited the code to use it.

--- main.py
+++ main.py
@@ -4,16 +4,36 @@ from datetime import datetime
 
 from get_github_trends import GithubTrend
 from get_dailydev_stream import DailyDevScraper
+from get_medium_recommended import MediumScraper
 from integration_notion_api import NotionAPI
+from json_database import JsonDatabase
 
-YOUR_NOTIONAPI_TOKEN =  "YOUR_NOTIONAPI_TOKEN"
-YOUR_NOTIONAPI_ACCESS_PAGE_ID = "YOUR_NOTIONAPI_ACCESS_PAGE_ID"
-YOUR_DAILY_DEV_COOKIE = "YOUR_DAILY_DEV_COOKIE"
+YOUR_NOTIONAPI_TOKEN =  ""
+YOUR_NOTIONAPI_ACCESS_PAGE_ID = ""
+YOUR_DAILY_DEV_COOKIE = ""
 
 def load_json(file_path):
     with open(file_path) as f:
         data = json.load(f)
     return data
+
+def get_database_id(search_platform, database_template, notion_api: NotionAPI):
+    # Load JSON templates
+    db = JsonDatabase('data.json')
+    try:
+        id = db.find_data(search_platform).pop()["page_id"]
+        database_id = id
+    except IndexError:
+        # Create database
+        database_response = notion_api.create_database(database_template)
+        if not database_response:
+            print("Failed to create database")
+            return None
+        db.write_data(database_response["id"], search_platform)
+        # Get the ID of the newly created database
+        database_id = database_response["id"]
+        print("New database created for {}: {}".format(search_platform, database_response["id"]))
+    return database_id
 
 def create_github_trends(notion_api):
     # Get current date
@@ -27,15 +47,8 @@ def create_github_trends(notion_api):
 
     # Replace the Notion API access page ID
     database_template["parent"]["page_id"] = YOUR_NOTIONAPI_ACCESS_PAGE_ID
-
-    # Create the weekly database
-    weekly_database_response = notion_api.create_database(database_template)
-    if not weekly_database_response:
-        print("Failed to create weekly database")
-        return
-
-    # Get the ID of the newly created weekly database
-    weekly_database_id = weekly_database_response["id"]
+    
+    weekly_database_id = get_database_id("github", database_template,notion_api)
 
     # Replace the database_id value with a new ID for the weekly page
     page_template["parent"]["database_id"] = weekly_database_id
@@ -84,31 +97,23 @@ def create_github_trends(notion_api):
 
 def create_dailydev_stream(notion_api):
     current_date = datetime.now().strftime("%Y-%m-%d")
-    
+
     # Load JSON templates
-    database_json_file = "Json/dailydev/dailydev_stream_database.json"
-    page_json_file = "Json/dailydev/dailydev_stream_page.json"
-    repos_database_json_file = "Json/dailydev/dailydev_stream_repos_database.json"
-    repos_page_json_file = "Json/dailydev/dailydev_stream_repos_page.json"
-    database_json = load_json(database_json_file)
-    page_json = load_json(page_json_file)
-    repos_database_json = load_json(repos_database_json_file)
-    repos_page_json = load_json(repos_page_json_file)
+    database_json = load_json("Json/dailydev/dailydev_stream_database.json")
+    page_json = load_json("Json/dailydev/dailydev_stream_page.json")
+    repos_database_json = load_json("Json/dailydev/dailydev_stream_repos_database.json")
+    repos_page_json = load_json("Json/dailydev/dailydev_stream_repos_page.json")
 
     # Replace necessary values in JSON files
     database_json["parent"]["page_id"] = YOUR_NOTIONAPI_ACCESS_PAGE_ID
     database_json["title"][0]["text"]["content"] = "Daily Dev Stream"
     page_json["properties"]["Date"]["date"]["start"] = current_date
     page_json["properties"]["Name"]["title"][0]["text"]["content"] = f"Trending Stream on {current_date}"
-    
-    # Create database
-    database_response = notion_api.create_database(database_json)
-    if not database_response:
-        print("Failed to create database")
-        return
+
+    # Get database ID
+    database_id = get_database_id("daily_dev", database_json,notion_api)
     
     # Get database ID and create page
-    database_id = database_response["id"]
     page_json["parent"]["database_id"] = database_id
     page_response = notion_api.create_page(page_json)
     if not page_response:
@@ -143,9 +148,71 @@ def create_dailydev_stream(notion_api):
         repos_page_json["properties"]["Tags"]["rich_text"][0]["text"]["content"] = ', '.join(node.get('tags', []))
         repos_page_json["properties"]["Type"]["rich_text"][0]["text"]["content"] = node.get('type') or ""
         repos_page_json["properties"]["Link"]["url"] = node.get('permalink') or ""
-        repos_page_json["properties"]["Created_Date"]["date"]["start"] = node.get('created_at', '').split('T')[0] or ""
+        repos_page_json["properties"]["Created_Date"]["date"]["start"] = node.get('createdAt','').split('T')[0] or ""
         repos_page_json["properties"]["Image"]["files"][0]["external"]["url"] = node.get('image') or ""
         notion_api.create_page(repos_page_json)
+        time.sleep(0.1)
+
+def create_medium_recommend(notion_api):
+    # Get current date
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    # Load JSON templates
+    database_template = load_json("Json/medium/medium_recommend_database.json")
+    page_template = load_json("Json/medium/medium_recommend_page.json")
+    repos_database_template = load_json("Json/medium/medium_recommend_repos_database.json")
+    repos_page_template = load_json("Json/medium/medium_recommend_repos_page.json")
+
+    # Replace the Notion API access page ID
+    database_template["parent"]["page_id"] = YOUR_NOTIONAPI_ACCESS_PAGE_ID
+
+    # Get database ID
+    recommend_database_id = get_database_id("medium", database_template, notion_api)
+
+    # Replace the database_id value with a new ID for the weekly page
+    page_template["parent"]["database_id"] = recommend_database_id
+    page_template["properties"]["Date"]["date"]["start"] = current_date
+    page_template["properties"]["Name"]["title"][0]["text"]["content"] = f"Recommend Articles on {current_date}"
+
+    # Create recommend page
+    recommend_page_response = notion_api.create_page(page_template)
+    if not recommend_page_response:
+        print("Failed to create weekly page")
+        return
+
+    # Get the ID of the newly created weekly page
+    recommend_page_id = recommend_page_response["id"]
+
+    # Replace the page_id value with a new ID for the weekly repos database
+    repos_database_template["parent"]["page_id"] = recommend_page_id
+    repos_database_template["title"][0]["text"]["content"] = "Medium Recommend Articles"
+
+    # Create recommend repos database
+    repos_database_response = notion_api.create_database(repos_database_template)
+    if not repos_database_response:
+        print("Failed to create weekly repos database")
+        return
+
+    # Fetch recommend articles from Medium
+    medium_scraper = MediumScraper()
+    medium_scraper.scrape()
+
+    # Load response data
+    response_data = load_json(f"Json/medium/medium_recommended_{current_date}.json")
+    items = response_data
+    # Create weekly repos page
+    for item in items:
+        repos_page_template["parent"]["database_id"] = repos_database_response["id"]
+        repos_page_template["properties"]["Name"]["title"][0]["text"]["content"] = item['name']
+        repos_page_template["properties"]["Reason"]["rich_text"][0]["text"]["content"] = item.get('reason') or ""
+        repos_page_template["properties"]["Creator_Name"]["rich_text"][0]["text"]["content"] = item['username']
+        repos_page_template["properties"]["Clap_Count"]["number"] = item['clapCount']
+        repos_page_template["properties"]["Reading_Time"]["number"] =round(item['readingTime'], 2) 
+        repos_page_template["properties"]["Tags"]["rich_text"][0]["text"]["content"] = ', '.join(item.get('tags', []))
+        repos_page_template["properties"]["Link"]["url"] = item['mediumUrl']
+        repos_page_template["properties"]["Published_Date"]["date"]["start"] = datetime.fromtimestamp(item['date']/1000.0).strftime('%Y-%m-%d %H:%M:%S') 
+        repos_page_template["properties"]["Image"]["files"][0]["external"]["url"] = item['previewImage']
+        notion_api.create_page(repos_page_template)
         time.sleep(0.1)
 
 if __name__ == "__main__":
@@ -160,3 +227,6 @@ if __name__ == "__main__":
 
     # Create dailydev stream
     create_dailydev_stream(notion_api)
+
+    # Create medium recommend
+    create_medium_recommend(notion_api)
